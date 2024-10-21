9cf0260ada9cab1c313f3d82894539b3fb2a7781
JustMenarh <amusaaminat@gmail.com>
2024-10-21 10:54:43 -0400

    initial commit

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<gdzJELZ6EWodFc1azxce270f>172.21.220.242:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
